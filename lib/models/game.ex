d23635da258627976258a05f9f86912708cf8b06
DainisL <dainis186@gmail.com>
2015-01-06 11:51:31 +0200

    add reate method to games model

--- lib/models/game.ex
+++ lib/models/game.ex
@@ -1,5 +1,8 @@
 defmodule Models.Game do
+  alias TableSoccer.Db.Repo, as: Repo
+
   use Ecto.Model
+  validate game, player_l_id: present(), player_r_id: present()
 
   schema "games" do
     belongs_to :player_l, Models.Player, foreign_key: :player_l_id
@@ -8,5 +11,19 @@ defmodule Models.Game do
     field :score_r, :integer, default: 0
     field :created_at, :datetime, default: Ecto.DateTime.local
     field :updated_at, :datetime, default: Ecto.DateTime.local
+  end
+
+   def create(attrs) when is_map(attrs) do
+    item = Map.merge(%__MODULE__{}, attrs)
+    case __MODULE__.validate(item) do
+      nil ->
+        Repo.insert(item)
+      errors ->
+        "Errors #{ inspect errors}"
+    end
+  end
+
+  def create(attrs) do
+    "attrs need to be map %{}"
   end
 end
